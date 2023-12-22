df900f1e78dc903a30412e09e419069497760a7d
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2023-12-22 15:31:37 +0100

    partie 3 et partie 4 du tp3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListeTachesComponent } from './components/liste-taches/liste-taches.component';
+import { NouvelleTacheComponent } from './components/nouvelle-tache/nouvelle-tache.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'list-taches',component:ListeTachesComponent },
+  { path : 'nouvelle-tache',component:NouvelleTacheComponent },
+  { path : '' , redirectTo : '/nouvelle-tache' , pathMatch :'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
